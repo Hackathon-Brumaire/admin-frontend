ba19ccaf2a8a2a21ee04cf777247cf6de5c11e06
RemyMach <remygt@hotmail.fr>
2022-03-11 04:16:58 +0100

    feat(visuel): Add home page

--- src/app/features/decision-tree/decision-tree.module.ts
+++ src/app/features/decision-tree/decision-tree.module.ts
@@ -3,11 +3,15 @@ import {CommonModule} from '@angular/common';
 import {MainComponent} from './components/main/main.component';
 import {DecisionTreeRoutingModule} from "./decision-tree-routing.module";
 import {SharedModule} from "../../shared/shared.module";
+import { HomeComponent } from './components/home/home.component';
+import { HeaderComponent } from './components/header/header.component';
 
 
 @NgModule({
   declarations: [
-    MainComponent
+    MainComponent,
+    HomeComponent,
+    HeaderComponent
   ],
   imports: [
     DecisionTreeRoutingModule,
